78ed881db769d2cdac08e8ea010877e90f95c3f5
Tech4All <tech4all583@gmail.com>
2021-02-26 14:05:58 +0100

    initial

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tech4all
+ * Date: 2/9/21
+ * Time: 9:00 AM
+ */
+
+require_once 'config/core.php';
+
+?>
+<!Doctype html>
+<html>
+<head>
+    <title>Admin</title>
+</head>
+<body>
+
+
+</body>
+</html>
